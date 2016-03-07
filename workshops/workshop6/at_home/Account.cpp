e233647297e54c01e793b5fdfc96bf7cad700f29
unknown <sashrafi3@SY-T2109-15.SeneDS.senecacollege.ca>
2016-03-07 14:57:19 -0500

    finished at home

--- workshops/workshop6/at_home/Account.cpp
+++ workshops/workshop6/at_home/Account.cpp
@@ -1,28 +1,70 @@
 #include <iomanip>
+#include <cstring>
 #include "Account.h"
+
 using namespace std;
-namespace sict{
-  Account::Account(){
-    name_[0] = 0;
-    balance_ = 0;
-  }
-  Account::Account(double balance){
-    name_[0] = 0;
-    balance_ = balance;
-  }
-  Account::Account(const char name[], double balance){
-    strncpy(name_, name, 40);
-    name_[40] = 0;
-    balance_ = balance;
-  }
 
+namespace sict
+{
 
-  void Account::display(bool gotoNewline)const{
-    cout << (name_[0] ? name_: "No Name") << ": $" << setprecision(2) << fixed << balance_;
-    if (gotoNewline) cout << endl;
-  }
+	Account::Account()
+	{
+		name_[0] = 0;
+		balance_ = 0;
+	}
 
+	Account::Account(double balance)
+	{
+		name_[0] = 0;
+		balance_ = balance;
+	}
 
+	Account::Account(const char name[], double balance)
+	{
+		strncpy(name_, name, 40);
+		name_[40] = 0;
+		balance_ = balance;
+	}
 
+	void Account::display(bool gotoNewline)const
+	{
+		cout << (name_[0] ? name_ : "No Name") << ": $" << setprecision(2) << fixed << balance_;
+		if (gotoNewline) cout << endl;
+	}
 
+	Account& Account::operator+=(const Account& other) {
+
+		balance_ += other.balance_;
+
+		return *this;
+	}
+
+	double operator+=(double& d, const Account& other)
+	{
+
+		d += other.balance_;
+		return d;
+	}
+
+	Account& Account::operator=(const char* name) {
+
+		strcpy(name_, name);
+
+		return *this;
+	}
+
+	Account operator+(const Account& other, const Account& other2) {
+
+		Account ret;
+		ret.name_[0] = '\0';
+		ret.balance_ = other.balance_ + other2.balance_;
+
+		return ret;
+	}
+
+	std::ostream& operator<<(std::ostream& os, const Account& acc)
+	{
+		acc.display(false);
+		return os;
+	}
 }
--- workshops/workshop6/at_home/w6_at_home.cpp
+++ workshops/workshop6/at_home/w6_at_home.cpp
@@ -9,34 +9,39 @@
 
 #include <iostream>
 #include "Account.h"
+
 using namespace sict;
 using namespace std;
-void displayABC(const Account& A,
-  const Account& B,
-  const Account& C){
-  cout << "A: " << A << endl << "B: " << B << endl
-    << "C: " << C << endl << "--------" << endl;
+
+void displayABC(const Account& A, const Account& B, const Account& C)
+{
+	cout << "A: " << A << endl << "B: " << B << endl
+		<< "C: " << C << endl << "--------" << endl;
 }
-int main(){
-  Account A;
-  Account B("Saving", 10000.99);
-  Account C("Checking", 100.99);
-  Account* AC[3] = { &A, &B, &C };
-  double balance = 0;
-  displayABC(A, B, C);
-  A = B + C;
-  displayABC(A, B, C);
-  A = "Joint";
-  displayABC(A, B, C);
-  A = B += C;
-  displayABC(A, B, C);
-  A = B += C += 100.01;
-  displayABC(A, B, C);
-  for (int i = 0; i < 3; i++){
-    cout << i+1 << "- " << (balance += *AC[i]) << endl;
-  }
-  cout << "Total Balance: " << balance << endl;
-  return 0;
+
+int main()
+{
+	Account A;
+	Account B("Saving", 10000.99);
+	Account C("Checking", 100.99);
+	Account* AC[3] = { &A, &B, &C };
+
+	double balance = 0;
+	displayABC(A, B, C);
+	A = B + C;
+	displayABC(A, B, C);
+	A = "Joint";
+	displayABC(A, B, C);
+	A = B += C;
+	displayABC(A, B, C);
+	A = B += C += 100.01;
+	displayABC(A, B, C);
+	for (int i = 0; i < 3; i++){
+		cout << i + 1 << "- " << (balance += *AC[i]) << endl;
+	}
+	cout << "Total Balance: " << balance << endl;
+
+	return 0;
 }
 
 /* output
