efbea8b91a8a634013ab1f08048c1ded7b713d53
unknown <sashrafi3@SY-T2110-W821.SeneDS.senecacollege.ca>
2016-03-09 08:48:24 -0500

    added prototypes in date.h

--- assignment/milestone1/Date.h
+++ assignment/milestone1/Date.h
@@ -8,11 +8,36 @@ namespace sict
 	class Date
 	{
 	private:
-		int value()const;
-	public:
+		int year_;
+		int mon_;
+		int day_;
+		int readErrorCode_;	// holds an error code to validify the date value
 
-		int mdays()const;
+		int value()const;	// returns a unique integer based on the date, used to compare two dates
+		void errCode(int errorCode);	// sets readErrorCode_ to a valid value
+	
+	public:
+		Date();
+		Date(int year, int month, int day);
+
+		bool operator==(const Date& D)const;
+		bool operator!=(const Date& D)const;
+		bool operator<(const Date& D)const;
+		bool operator>(const Date& D)const;
+		bool operator<=(const Date& D)const;
+		bool operator>=(const Date& D)const;
+
+		int mdays(int mon)const;	// returns number of days in a month
+		int errCode()const;	// returns the readErrorCode_ value
+		bool bad()const;	// returns true if readErrorCode_ is not zero
+
+		std::istream& read(std::istream& istr);	// reads date in format: YYY?MM?DD
+		std::ostream& write(std::ostream& ostr)const;	// writes the date in format: YYYY/MM/DD
 	};
+
+	// custom IO for date
+	std::istream& operator>>(std::istream& istr, const Date& d);
+	std::ostream& operator<<(std::ostream& ostr, const Date& d);
 }
 
 #endif
