1c7818bf2d9d2a7d098bd54b5bf91de9fbd028b6
unknown <sashrafi3@SY-T2108-W829.SeneDS.senecacollege.ca>
2016-03-16 12:04:26 -0400

    finished milestone 2

--- assignment/milestone2/ErrorMessage.h
+++ assignment/milestone2/ErrorMessage.h
@@ -1,11 +1,30 @@
 #ifndef SICT_ERRORMESSAGE_H__
 #define SICT_ERRORMESSAGE_H__
+
 #include <iostream>
-namespace sict{
 
+namespace sict
+{
+	class ErrorMessage
+	{
+		char* message_;
 
+	public:
+		ErrorMessage();
+		ErrorMessage(const char* errorMessage);
+		
+		ErrorMessage(const ErrorMessage& em) = delete;	// prevent copying of an errormessage object
+		ErrorMessage& operator=(const ErrorMessage& em) = delete;	// prevent assigning an errormessage object to another
+		ErrorMessage& operator=(const char* errorMessage);
+		virtual ~ErrorMessage();
+		
+		void clear();
+		bool isClear() const;
+		void message(const char* value);
+		const char* message() const;
+	};
 
-
+	std::ostream& operator<<(std::ostream& ostr, const ErrorMessage& err);
 }
-#endif
 
+#endif
