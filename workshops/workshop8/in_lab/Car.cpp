8ac22806756ce377151b8ce2500a458ae0ed1711
sorash <so.as67@yahoo.com>
2016-04-07 00:10:45 -0400

    finished car class

--- workshops/workshop8/in_lab/Car.cpp
+++ workshops/workshop8/in_lab/Car.cpp
@@ -1,7 +1,25 @@
 #include "Car.h"
-namespace sict{
 
+namespace sict
+{
+	void Car::speed(int value)
+	{
+		speed_ = value;
+	}
 
+	int Car::maxSpeed() const
+	{
+		return maxSpeed_;
+	}
 
+	int Car::speed() const
+	{
+		return speed_;
+	}
 
+	Car::Car(int max)
+	{
+		maxSpeed_ = max;
+		speed_ = 0;
+	}
 }
