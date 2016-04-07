cf2d1ceca2f2f00c9c3d11a76df183a62a1ef53a
sorash <so.as67@yahoo.com>
2016-04-07 00:17:43 -0400

    finished sport car class

--- workshops/workshop8/in_lab/SportCar.cpp
+++ workshops/workshop8/in_lab/SportCar.cpp
@@ -1,7 +1,32 @@
 #include "SportCar.h"
 
-namespace sict{
+namespace sict
+{
+	SportCar::SportCar()
+	{
+		noOfPassengers_ = 0;
+	}
 
+	SportCar::SportCar(int max, int passNo) : Car(max)
+	{
+		noOfPassengers_ = passNo;
+	}
 
+	void SportCar::accelerate()
+	{
+		speed(speed() + 40);
+	}
 
+	void SportCar::brake()
+	{
+		speed(speed() - 10);
+	}
+
+	std::ostream& SportCar::display(std::ostream& ostr) const
+	{
+		if (speed() > 0)
+			ostr << "This sport car is carrying " << noOfPassengers_ << " and is traveling at a speed of " << speed() << " km/h.";
+		if (speed() == 0)
+			ostr << "This sport car is carrying " << noOfPassengers_ << " and is parked.";
+	}
 }
--- workshops/workshop8/in_lab/SportCar.h
+++ workshops/workshop8/in_lab/SportCar.h
@@ -1,13 +1,22 @@
 #ifndef SICT_SPORTCAR_H_
 #define SICT_SPORTCAR_H_
 
-namespace sict{
-  class SportCar ???  {
-  public:
+#include "Car.h"
 
-    // implementations of Vehicle's pure virtual methods
+namespace sict
+{
+	class SportCar : public Car
+	{
+		int noOfPassengers_;
 
+	public:
+		SportCar();
+		SportCar(int max, int passNo);
 
-  };
+		// implementations of Vehicle's pure virtual methods
+		void accelerate();
+		void brake();
+		std::ostream& display(std::ostream& ostr) const;
+	};
 }
 #endif
