2015c558d1a532ecd5359ccfc4b875ec610c3efc
sorash <so.as67@yahoo.com>
2016-04-07 01:14:21 -0400

    finished functions in main class

--- workshops/workshop7/in_lab/w7_in_lab.cpp
+++ workshops/workshop7/in_lab/w7_in_lab.cpp
@@ -7,7 +7,8 @@ using namespace sict;
 void drive(Car& c)
 {
 	c.accelerate();
-	c.display(std::cout);
+	c.brake();
+	c.display(cout) << endl;
 }
 
 void stop(Car& c)
@@ -15,24 +16,24 @@ void stop(Car& c)
 	while (c.speed() > 0)
 		c.brake();
 
-	c.display(std::cout);
+	c.display(cout) << endl;
 }
 
 int main()
 {
-  SportCar Tesla(240, 2);
-  PickupTruck Ford;
-  Tesla.display(cout) << endl;
-  Ford.display(cout) << endl;
-  Ford.load("Bricks", 3500);
-  drive(Tesla);
-  drive(Ford);
-  stop(Tesla);
-  stop(Ford);
-  Ford.unload();
-  Tesla.display(cout) << endl;
-  Ford.display(cout) << endl;
-  return 0;
+	SportCar Tesla(240, 2);
+	PickupTruck Ford;
+	Tesla.display(cout) << endl;
+	Ford.display(cout) << endl;
+	Ford.load("Bricks", 3500);
+	drive(Tesla);
+	drive(Ford);
+	stop(Tesla);
+	stop(Ford);
+	Ford.unload();
+	Tesla.display(cout) << endl;
+	Ford.display(cout) << endl;
+	return 0;
 }
 /* output
 This sport car is carrying 2 passengers and is parked.
